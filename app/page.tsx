aefffbecc01b1ba55d7508df2e0560646ba93ff6
Railan002 <sophiawriter56@gmail.com>
2025-05-09 00:02:54 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Chibaby's Sports Prediction Site</h1>
+      <p>High probability games will appear here soon.</p>
+    </main>
+  );
+}
